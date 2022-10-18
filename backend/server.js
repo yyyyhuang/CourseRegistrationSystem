8886a200e78d6297671f38760a1f58d2be6bd911
yingjhuang <huang.yingj@northeastern.edu>
2022-10-17 22:44:52 -0700

    backend server

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from 'cors';
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json);
+
+app.use("api/v1/khouryregister", register);
+app.use("*", (req, res) => {
+    res.status(404).json({error: "not found"});
+})
+
+export default app;
